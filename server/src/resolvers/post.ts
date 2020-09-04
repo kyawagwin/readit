10655473001e7b47d40996928ca67e91c5989052
Kyaw Aung Win <kyawagwin@gmail.com>
2020-09-04 20:38:08 +0800

    posts pagination

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -5,17 +5,34 @@ import {
   Arg,
   Mutation,
   UseMiddleware,
+  Int,
 } from "type-graphql";
 import { Post } from "../entities/Post";
 import { MyContext } from "../types";
 import { PostInput } from "./PostInput";
 import { isAuth } from "../middleware/isAuth";
+import { getConnection } from "typeorm";
 
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
-  async posts(): Promise<Post[]> {
-    return await Post.find();
+  async posts(
+    @Arg("limit", () => Int) limit: number,
+    @Arg("cursor", () => String, { nullable: true }) cursor: string | null
+  ): Promise<Post[]> {
+    const realLimit = Math.min(50, limit);
+    const qb = getConnection()
+      .getRepository(Post)
+      .createQueryBuilder("p")
+      .orderBy('"createdAt"', "DESC")
+      .take(realLimit);
+
+    if (cursor)
+      qb.where('"createdAt" < :cursor', {
+        cursor: new Date(parseInt(cursor)),
+      });
+
+    return qb.getMany();
   }
 
   @Query(() => Post, { nullable: true })
